d1db0d41f3659ee8ec8903761ed0967b67f23766
lilygithub2000 <zhaoli_lucy@163.com>
2021-07-14 18:26:07 +0800

    fail test

--- src/main/java/com/zl/CheckAccount.java
+++ src/main/java/com/zl/CheckAccount.java
@@ -17,27 +17,6 @@ public class CheckAccount extends HttpServlet {
         doGet(req,resp);
     }
 
-    @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        HttpSession session = req.getSession();
-        AccountBean account = new AccountBean();
-        String username = req.getParameter("username");
-        String pwd = req.getParameter("pwd");
-        account.setPassword(pwd);
-        account.setUsername(username);
-        if((username != null)&&(username.trim().equals("jsp"))) {
-            if((pwd != null)&&(pwd.trim().equals("1"))) {
-                System.out.println("success");
-                session.setAttribute("account", account);
-                String login_suc = "success.jsp";
-                resp.sendRedirect(login_suc);
-                return;
-            }
-        }
-        String login_fail = "fail.jsp";
-        resp.sendRedirect(login_fail);
-        return;
-    }
+   
 
 }
